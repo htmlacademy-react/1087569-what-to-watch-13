4e8019dc2f8253621dda968282282a79c753d8ce
SneG95 <Dima6233@yandex.ru>
2023-09-28 18:35:20 +0300

    Реализует регистрацию

--- src/pages/login-screen/login-screen.tsx
+++ src/pages/login-screen/login-screen.tsx
@@ -1,8 +1,26 @@
 import Logo from '../../components/logo/logo';
 import Footer from '../../components/footer/footer';
 import { Helmet } from 'react-helmet-async';
+import { useRef, FormEvent } from 'react';
+import { useAppDispatch } from '../../hooks';
+import { loginAction } from '../../store/api-actions';
 
 function LoginScreen(): JSX.Element {
+  const emailRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+  const dispatch = useAppDispatch();
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    if (emailRef.current !== null && passwordRef.current !== null) {
+      dispatch(loginAction({
+        email: emailRef.current.value,
+        password: passwordRef.current.value
+      }));
+    }
+  };
+
   return (
     <div className="user-page">
       <Helmet>
@@ -14,14 +32,34 @@ function LoginScreen(): JSX.Element {
       </header>
 
       <div className="sign-in user-page__content">
-        <form action="#" className="sign-in__form">
+        <form
+          onSubmit={handleSubmit}
+          action="#"
+          className="sign-in__form"
+        >
           <div className="sign-in__fields">
             <div className="sign-in__field">
-              <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" />
+              <input
+                ref={emailRef}
+                className="sign-in__input"
+                type="email"
+                placeholder="Email address"
+                name="user-email"
+                id="user-email"
+                required
+              />
               <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
             </div>
             <div className="sign-in__field">
-              <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" />
+              <input
+                ref={passwordRef}
+                className="sign-in__input"
+                type="password"
+                placeholder="Password"
+                name="user-password"
+                id="user-password"
+                required
+              />
               <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
             </div>
           </div>
